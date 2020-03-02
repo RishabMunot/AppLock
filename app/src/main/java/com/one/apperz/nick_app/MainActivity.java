5c03d6b7b3b09cf806e1264fcae6a9ff9000ed59
RishabMunot <rishabmunot.az@gmail.com>
2020-03-02 16:40:08 +0530

    List of app is generated which is displayed in recylerview. Click functionality is not set up and also lock mechanism is not set.

--- app/src/main/java/com/one/apperz/nick_app/MainActivity.java
+++ app/src/main/java/com/one/apperz/nick_app/MainActivity.java
@@ -1,8 +1,23 @@
 package com.one.apperz.nick_app;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.one.apperz.nick_app.adapters.AppAdapters;
+import com.one.apperz.nick_app.model.AppItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initView();
+
+    }
+
+    private void initView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        AppAdapters appAdapters = new AppAdapters(this, getAllApps());
+        recyclerView.setAdapter(appAdapters);
+
+    }
+
+    private List<AppItem> getAllApps() {
+
+        List<AppItem> results = new ArrayList<>();
+
+        PackageManager pk = getPackageManager();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> resolveInfos = pk.queryIntentActivities(intent, 0);
+
+        for (ResolveInfo reIN : resolveInfos) {
+            ActivityInfo activityInfo = reIN.activityInfo;
+            results.add(new AppItem(activityInfo.loadIcon(pk), activityInfo.loadLabel(pk).toString(), activityInfo.packageName));
+        }
+
+        return results;
+    }
+
+    private void initToolbar() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        getSupportActionBar().setTitle("App List");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home)
+            finish();
+
+
+        return true;
     }
 }
