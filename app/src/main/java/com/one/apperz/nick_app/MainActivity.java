ed60d39b77d6f9e19f6a93c9b14caf19579b57ec
RishabMunot <rishabmunot.az@gmail.com>
2020-03-02 17:57:47 +0530

    Enabled Package Usage Stats

--- app/src/main/java/com/one/apperz/nick_app/MainActivity.java
+++ app/src/main/java/com/one/apperz/nick_app/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,11 +19,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.one.apperz.nick_app.adapters.AppAdapters;
 import com.one.apperz.nick_app.model.AppItem;
+import com.one.apperz.nick_app.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout layout_permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         AppAdapters appAdapters = new AppAdapters(this, getAllApps());
         recyclerView.setAdapter(appAdapters);
+
+        layout_permission = findViewById(R.id.layout_prmission);
 
     }
 
@@ -82,5 +91,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         return true;
+    }
+
+    public void setPermission(View view) {
+        startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+    }
+
+    @Override
+    protected void onResume() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (Utils.checkPermission(this)) {
+                layout_permission.setVisibility(View.GONE);
+            } else {
+                layout_permission.setVisibility(View.VISIBLE);
+            }
+        }
+
+        super.onResume();
     }
 }
